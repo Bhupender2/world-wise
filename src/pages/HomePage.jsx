7bec21bb1875cd825a5e32c9245e275a0e4e506d
Bhupender2 <b.mehtaa2021@gmail.com>
2024-03-29 02:52:35 +0530

    homepage is not reflecting the changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 import styles from "./Homepage.module.css";
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
+    <PageNav/>
       <section>
         <h1>
           You travel the world.
@@ -13,9 +15,11 @@ export default function Homepage() {
         <h2>
           A world map that tracks your footsteps into every city you can think
           of. Never forget your wonderful experiences, and show your friends how
-          you have wandered the world.
+          you have wandered the world
         </h2>
-        <Link to="/app" className="cta" >Start Tracking Now</Link>
+        <Link to="/app" className="cta">
+          Start Tracking Noww
+        </Link>
       </section>
     </main>
   );
