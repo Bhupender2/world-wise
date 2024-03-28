58fc19f1e5255b2cdc88e9d89831369a27911623
Bhupender2 <b.mehtaa2021@gmail.com>
2024-03-29 03:51:01 +0530

    logo defined products, pricing, login page done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import AppLayout from "./pages/AppLayout"
+import Login from "./pages/Login";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
+import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
-
+  
   return (
     <main className={styles.login}>
+      <PageNav />
       <form className={styles.form}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
