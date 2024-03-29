935f031212e543cae0d3d14af73b5dd8af033d16
Bhupender2 <b.mehtaa2021@gmail.com>
2024-03-29 23:34:45 +0530

    footer component , sidebar comp and module made , map comp made

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,12 @@
-
-
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
-     AppLayout
+      <Sidebar />
+      <Map/>
     </div>
   );
 }
