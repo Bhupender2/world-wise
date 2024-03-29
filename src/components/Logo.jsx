e3ff6a13c84e003157252dca934d49e3d967d0b7
Bhupender2 <b.mehtaa2021@gmail.com>
2024-03-29 20:54:41 +0530

    starter comoonent files added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,7 @@
-import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
 
 function Logo() {
-  return (
-    <Link to="/">
-      <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
-    </Link>
-  );
+  return <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />;
 }
 
 export default Logo;
