aaae70cab1ae77f769d290cc74895667b1fda5d0
Bhupender2 <b.mehtaa2021@gmail.com>
2024-03-30 07:08:03 +0530

    cities data is fetched on app component

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,32 @@ import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import { useEffect, useState } from "react";
+import CityList from "./components/CityList";
+
+const BASE_URL = "http://localhost:9000";
 
 export default function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    //we want to see the data on mount and fetching data is a side effect so we use useEffect
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        console.log(data)
+        setCities(data);
+      } catch {
+        console.log("data is not fetched ");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
@@ -15,9 +39,9 @@ export default function App() {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/app" element={<AppLayout />}>
-          <Route index element={<p>List of the cities</p>} />
+          <Route index element={<CityList/>} />
           {/* it is render is there is no nested routes its the default*/}
-          <Route path="cities" element={<p>List of the cities</p>} />
+          <Route path="cities" element={<CityList/>} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,7 @@
+import styles from "./CityList.module.css";
+
+function CityList() {
+  return <div className={styles.cityList}>LIST</div>;
+}
+
+export default CityList;
