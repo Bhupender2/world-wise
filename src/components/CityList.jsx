f0e8ea47a3d53fc985b5e4b30a9180b4e7359697
Bhupender2 <b.mehtaa2021@gmail.com>
2024-03-30 10:02:25 +0530

    cityList component completed

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,13 @@
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
+import Message from "./Message";
 
 function CityList({ isLoading, cities }) {
   if (isLoading) return <Spinner />;
+
+  if (!cities.length) //if cities data is not fetched then this will do the work
+    return <Message message="Add your city by clicking on a city on the map" />;
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
