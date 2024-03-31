49da7899e102a9d28a6d8c0ee907fd927bae3167
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-01 01:01:10 +0530

    usage of params done (to store ui state in the URL)

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -49,6 +50,7 @@ export default function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City/>}/>
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
