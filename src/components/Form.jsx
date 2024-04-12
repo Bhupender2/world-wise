9691c0b46f63e426fd36ac715622159ba4f18012
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-12 18:03:22 +0530

    using lat, lng position we retrieve data of city,country ,emoji using fetching data from reverse-geocode url and use it in the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,8 @@ import BackButton from "./BackButton";
 import Button from "./Button";
 
 import styles from "./Form.module.css";
+import Message from "./Message";
+import Spinner from "./Spinner";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -15,7 +17,7 @@ export function convertToEmoji(countryCode) {
   return String.fromCodePoint(...codePoints);
 }
 
-const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client"; // to get the info about that particular position
 function Form() {
   const [isLoadingGeoCoding, setIsLoadingGeoCoding] = useState(false);
   const [cityName, setCityName] = useState("");
@@ -24,19 +26,29 @@ function Form() {
   const [notes, setNotes] = useState("");
 
   const [lat, lng] = useUrlPosition();
+  const [emoji, setEmoji] = useState("");
+
+  const [geocodingError, setGeocodingError] = useState("");
 
   useEffect(() => {
     async function fetchCityData() {
       try {
         setIsLoadingGeoCoding(true);
+        setGeocodingError("");
         const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
         const data = await res.json();
+
+        if (!data.countryCode)
+          throw new Error(
+            "it deosn't seem to be a city click somewhere else 😢"
+          );
         setCityName(data.city || data.locality || "");
         setCountry(data.countryName);
+        setEmoji(convertToEmoji(data.countryCode));
 
         console.log(data);
       } catch (err) {
-    
+        setGeocodingError(err.message);
       } finally {
         setIsLoadingGeoCoding(false);
       }
@@ -44,42 +56,44 @@ function Form() {
     fetchCityData();
   }, [lat, lng]);
 
-  return (
-    <form className={styles.form}>
-      <div className={styles.row}>
-        <label htmlFor="cityName">{cityName}</label>
-        <input
-          id="cityName"
-          onChange={(e) => setCityName(e.target.value)}
-          value={cityName}
-        />
-        {/* <span className={styles.flag}>{emoji}</span> */}
-      </div>
+  if (geocodingError) return <Message message={geocodingError} />
+  if (isLoadingGeoCoding)  return <Spinner/>
+    return (
+      <form className={styles.form}>
+        <div className={styles.row}>
+          <label htmlFor="cityName">{cityName}</label>
+          <input
+            id="cityName"
+            onChange={(e) => setCityName(e.target.value)}
+            value={cityName}
+          />
+          <span className={`${styles.flag} ${styles.emoji}`}>{emoji}</span>
+        </div>
 
-      <div className={styles.row}>
-        <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
-          id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
-        />
-      </div>
+        <div className={styles.row}>
+          <label htmlFor="date">When did you go to {cityName}?</label>
+          <input
+            id="date"
+            onChange={(e) => setDate(e.target.value)}
+            value={date}
+          />
+        </div>
 
-      <div className={styles.row}>
-        <label htmlFor="notes">Notes about your trip to {cityName}</label>
-        <textarea
-          id="notes"
-          onChange={(e) => setNotes(e.target.value)}
-          value={notes}
-        />
-      </div>
+        <div className={styles.row}>
+          <label htmlFor="notes">Notes about your trip to {cityName}</label>
+          <textarea
+            id="notes"
+            onChange={(e) => setNotes(e.target.value)}
+            value={notes}
+          />
+        </div>
 
-      <div className={styles.buttons}>
-        <Button type="primary"> Add </Button>
-        <BackButton />
-      </div>
-    </form>
-  );
+        <div className={styles.buttons}>
+          <Button type="primary"> Add </Button>
+          <BackButton />
+        </div>
+      </form>
+    );
 }
 
 export default Form;
