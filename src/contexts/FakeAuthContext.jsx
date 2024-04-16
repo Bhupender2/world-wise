0b23ceeb97b1ec41722c857701d569f6c5b157fa
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-16 11:15:40 +0530

    useReducer function setup (not fully)

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,8 +1,27 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.payload) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+  }
+}
+
 function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   return <AuthContext.Provider>{children}</AuthContext.Provider>;
 }
 
