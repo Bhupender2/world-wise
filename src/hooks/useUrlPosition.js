0d692245664413c9f57c6a769d662deacca8af39
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-12 00:28:11 +0530

    setsearch params ko delete kardiya

--- src/hooks/useUrlPosition.js
+++ src/hooks/useUrlPosition.js
@@ -1,7 +1,7 @@
 import { useSearchParams } from "react-router-dom";
 
 export function useUrlPosition() {
-  const [searchParams, SetSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
   return [lat, lng];
