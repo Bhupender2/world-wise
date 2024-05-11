b9bc521fe975c7c7e9d5bd15029251d4d30e9bb4
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-11 07:27:54 +0530

    icon added and displayed on 430px width

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,10 @@
 import styles from "./Logo.module.css";
 
 function Logo() {
-  return <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />;
+  return <>
+    <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
+    <img src="/icon.png" alt="WorldWise logo" className={styles.logo_icon} />
+  </>;
 }
 
 export default Logo;
